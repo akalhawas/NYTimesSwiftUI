f80ae67bd0d5a3d789272dbc55e98cf2390d7d40
ali alhawas <alialhawas@alis-MacBook-Pro.local>
2024-04-23 14:42:15 -0700

    Creating NYTimeSwiftUI app and updating .gitignore file

---
+++ NYTimesSwiftUI/NYTimesSwiftUI/NYTimesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesSwiftUIApp.swift
+//  NYTimesSwiftUI
+//
+//  Created by ali alhawas on 14/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
