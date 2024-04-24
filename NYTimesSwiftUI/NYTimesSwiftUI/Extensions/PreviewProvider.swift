273fce38370e14bb71d57ce4ef385c0d20bcaddc
ali alhawas <alialhawas@alis-MacBook-Pro.local>
2024-04-24 14:27:50 -0700

    setup Unit Test + New Modifications Update comneted code

--- NYTimesSwiftUI/NYTimesSwiftUI/Extensions/PreviewProvider.swift
+++ NYTimesSwiftUI/NYTimesSwiftUI/Extensions/PreviewProvider.swift
@@ -7,12 +7,6 @@
 
 import SwiftUI
 
-//extension PreviewProvider {
-//    static var dev: DeveloperPreview {
-//        return DeveloperPreview.instance
-//    }
-//}
-
 class DeveloperPreview {
     static let instance = DeveloperPreview()
     private init() { }
